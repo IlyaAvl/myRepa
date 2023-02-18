8a659ca0f0a08660d66e2a1e66068e80bd328db9
IlyaAvl <121689937+IlyaAvl@users.noreply.github.com>
2023-02-18 14:36:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,28 @@
 
-// // // // // // // //  let num1=+prompt("Введите число 1");
-// // // // // // // //  let num2=+prompt("Введите число 2");
-// // // // // // // //  if (num1>num2)
-// // // // // // // //  {console.log(`${num1}>${num2}`);} 
-// // // // // // // // else if (num2>num1) {console.log(`${num1}<${num2}`);}
-// // // // // // // // // // // // else if (num1===num2) {console.log(`${num1}=${num2}`);}
-// // // // // // // // // // // // else {console.log(`ошибка ввода`);}
+// Задание 5
+const numbers = [2,5,3,9]
+const result = (numbers[0]*numbers[1]+numbers[2]*numbers[3]);
+console.log(result);
 
-// // // // // // // // // // // let name = prompt('Введите свое имя');
-// // // // // // // // // // // if(name ==='Евгений') {
-// // // // // // // // // // //     alert('Привет');
-// // // // // // // // // // // } else{
-// // // // // // // // // // //     alert('Не пущу');
-// // // // // // // // // // // }
+// Задание 7
+   const brands = ['adidas', 'nike', 'puma', 'demix', 'kappa',]
+   console.log(brands[brands.length-1]);
 
+// Задание 8
+  const arr = ['a','b','c']
+  console.log(arr[0]=1);
+  console.log(arr[1]=2);
+  console.log(arr[2]=-3);
 
-// // // // // // // // // // let num = prompt('введите число')
-// // // // // // // // // // switch (num) {
-// // // // // // // // // //     case '1':
-// // // // // // // // // //         alert('зима');
-// // // // // // // // // //         break;
-// // // // // // // // // //     case '2':
-// // // // // // // // // //         alert('весна');
-// // // // // // // // // //         break;
-// // // // // // // // // //     case '3':
-// // // // // // // // // //         alert('лето');
-// // // // // // // // // //         break;
-// // // // // // // // // //     case '4':
-// // // // // // // // // //         alert('осень');
-// // // // // // // // // //         break;
+// Задание 0
+ const numbers = [1,2,3,4];
+ for(let value of numbers){
+     value +=3;
+     console.log(value);
+ }
 
-// // // // // // // // // //     default:
-// // // // // // // // // //         alert('нет такой поры года');
-// // // // // // // // // // }
-
-// // // // // // // // // let num1=+prompt("Введите число 1");
-// // // // // // // // // let num2=+prompt("Введите число 2");
-// // // // // // // // // if (num1>num2){
-// // // // // // // // //     console.log('ошибка');
-// // // // // // // // // } else {
-// // // // // // // // //     console.log(num1, num2);
-// // // // // // // // // }
-
-// // // // // // // // let num1=+prompt("Введите число 1");
-// // // // // // // // let num2=+prompt("Введите число 2");
-// // // // // // // // let num3=+prompt("Введите число 3");
-// // // // // // // // console.log((num1+num2+num3),(num1*num2*num3));
-
-
-// // // // // // // let a=+prompt('введите число');
-// // // // // // // if(a<5 && a>0){
-// // // // // // //     console.log('верно');
-// // // // // // // } else{
-// // // // // // //     console.log('неверно');
-// // // // // // // }
-
-// // // // // const numbers = [2,5,3,9]
-// // // // // const result = (numbers[0]*numbers[1]+numbers[2]*numbers[3]);
-// // // // // console.log(result);
-
-// // // // const brands = ['adidas', 'nike', 'puma', 'demix', 'kappa',]
-// // // // console.log(brands[brands.length-1]);
-
-// // // const arr = ['a','b','c']
-// // // console.log(arr[0]=1);
-// // // console.log(arr[1]=2);
-// // // console.log(arr[2]=-3);
-
-// // const numbers = [1,2,3,4];
-// // for(let value of numbers){
-// //     value +=3;
-// //     console.log(value);
-// // }
-
-// const numbers = [1,2,3];
-// for(let i=2; i<=numbers.length+1; i++){
-//     console.log(i);
-// }
+// Задание 10
+ const numbers = [1,2,3];
+ for(let i=2; i<=numbers.length+1; i++){
+     console.log(i);
+ }
